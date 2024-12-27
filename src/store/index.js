8572bbda88fbfc5931802b9943de0a2f59321c3f
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-12-27 20:08:29 -0300

    feat: 314. working with multiple state properties

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,25 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialtate = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialtate, action) => {
   if (action.type === "INCREMENT") {
-    return { counter: state.counter + 1 };
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
   }
 
   if (action.type === "INCREASE") {
-    return { counter: state.counter + action.payload };
+    return { ...state, counter: state.counter + action.payload };
   }
 
   if (action.type === "DECREMENT") {
-    return { counter: state.counter - 1 };
+    return { ...state, counter: state.counter - 1 };
+  }
+
+  if (action.type === "TOGGLE") {
+    return { ...state, showCounter: !state.showCounter };
   }
 
   return state;
