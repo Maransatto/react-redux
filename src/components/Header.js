64e746ca04ccc4b9f6f3a5d0368f2da9b3137963
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-12-27 21:38:21 -0300

    feat: 322. Splitting Our code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store/auth";
 import classes from "./Header.module.css";
-import { authActions } from "../store";
 
 const Header = () => {
   const dispatch = useDispatch();
